4ddc9941088e6a51007a2b9a4ad71dd7500332ae
KAUANSANTOS <kauanauan71@gmail.com>
2022-03-15 00:49:54 -0300

    criando ambiente

---
+++ src/App.styles.ts
@@ -0,0 +1,25 @@
+//arquivo com estilos do projeto
+import styled from "styled-components";
+
+export const Container =styled.div`
+    background-color:#17181F;
+    color:787A81;
+    min-height:100vh;
+
+`
+export const Area = styled.div`
+    margin:auto;
+    max-width:980px;
+    padding:10px;
+`
+
+export const Header= styled.h1`
+    margin:0px;
+    padding:0px;
+    color:#fff;
+    text-align:center;
+    border-bottom: 1px solid #444;
+    padding-bottom:20px;
+    
+`
+
